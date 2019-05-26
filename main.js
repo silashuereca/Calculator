4cef8e782f027e4cec09c45b95867115d087fa4e
silashuereca <silasgranthuereca@gmail.com>
2019-05-26 15:40:29 -0500

    Updated js file functions

--- main.js
+++ main.js
@@ -1,46 +1,104 @@
+let displayString;
+let inputString;
+let a , b;
+let display = document.getElementById('output');
 
-let number = 4;
-let number2 = 4;
 
+// targeting all buttons 
+const container = document.getElementById('main-container')
+container.addEventListener('click', buttons);
 
-function add(num1, num2) {
-  return num1 + num2;
+function buttons(e) {
+  element = e.target.value
+  operator = e.target.key
+  inputHandler(element, operator)
 }
 
-function subtract(num1, num2) {
-  return num1 - num2;
+// loading in all button presses
+function inputHandler(element) {
+
+ if(element !== 'DEL' 
+    && element !== 'AC'
+    && element !== '/') {
+     console.log(display.value += element);
+    } 
+    
+    if(operator === '/') {
+      console.log(a = operator);
+    }
+
+  if(element === '=') {
+    operate(a, b, operator)
+  }
+
+ if(element === 'AC'){clearAll()}
+ if(element === 'DEL') {console.log(backSpace())}
+
+
+ 
 }
 
-function divide(num1, num2) {
-  return num1 / num2;
+//Clearing all content
+function clearAll() {
+  display.value = '';
 }
 
-function multiply(num1, num2) {
-  return num1 * num2;
+//Deleing last character in calculator
+function backSpace() {
+  display.value = display.value.substr(0 , display.value.length -1 )
 }
+
+// Storing values in display
+function updateDisplay(result) {
+  return console.log(display.value = result)
+}
+
+
+
+
+
+ //Calculating basic math functions here
+function add(num1, num2) {return num1 + num2;}
+function subtract(num1, num2) {return num1 - num2;}
+function divide(num1, num2) {return num1 / num2;}
+function multiply(num1, num2) {return num1 * num2;}
+
 
 // Calculating values here
 function operate(num1, num2, operator) {
   let a = num1;
   let b = num2;
+  let result;
 
   switch (operator) {
     case '+':
-      return console.log(add(a, b));
+     result = add(a, b);
     case '-':
-      return console.log(subtract(a , b));
-    case '/': 
-      return console.log(divide(a , b));
+     result = subtract(a , b);
+    case '/':
+     if(b != 0) {
+     result = divide(a , b);
+     } else {
+       'Error'
+     }
     case '*':
-      return console.log(multiply(a , b));
+     result = multiply(a , b);
     
   }
 
- 
-    
+    //passing in the result to be displayed
+   return console.log(updateDisplay(result)) 
 }
 
-  console.log(operate(number, number2, '*'));
   
+
+
+
+
+
+
+
+
+
 
 
